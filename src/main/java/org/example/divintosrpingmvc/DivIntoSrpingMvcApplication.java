0341bdcc7aefcdc7da3d8b5fd8e15fe6a1ebcf48
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-07 23:48:55 +0000

    insert some patients in database for the test

--- src/main/java/org/example/divintosrpingmvc/DivIntoSrpingMvcApplication.java
+++ src/main/java/org/example/divintosrpingmvc/DivIntoSrpingMvcApplication.java
@@ -1,13 +1,26 @@
 package org.example.divintosrpingmvc;
 
+import org.example.divintosrpingmvc.entities.Patient;
+import org.example.divintosrpingmvc.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class DivIntoSrpingMvcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DivIntoSrpingMvcApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+        return args -> {
+
+        };
     }
 
 }
