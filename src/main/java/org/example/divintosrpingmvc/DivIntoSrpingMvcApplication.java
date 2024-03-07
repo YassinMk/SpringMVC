f9553c55ba4c2b3ba76e265f2399f56b261f51ef
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-07 21:08:44 +0000

    prepare envirement

---
+++ src/main/java/org/example/divintosrpingmvc/DivIntoSrpingMvcApplication.java
@@ -0,0 +1,13 @@
+package org.example.divintosrpingmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivIntoSrpingMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DivIntoSrpingMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/divintosrpingmvc/DivIntoSrpingMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.divintosrpingmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivIntoSrpingMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
