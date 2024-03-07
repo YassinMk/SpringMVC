843939ff3689b1e45e75dfe12428288db33e21d5
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-07 23:24:35 +0000

    add methode to search patient by name

--- src/main/java/org/example/divintosrpingmvc/repositories/PatientRepository.java
+++ src/main/java/org/example/divintosrpingmvc/repositories/PatientRepository.java
@@ -1,7 +1,10 @@
 package org.example.divintosrpingmvc.repositories;
 
 import org.example.divintosrpingmvc.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNomContaining(String name, Pageable pageable);
 }
