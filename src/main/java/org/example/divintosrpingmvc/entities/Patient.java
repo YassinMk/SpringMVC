1234711b0367e8ae19065a90630c15378f39cd56
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-10 16:22:58 +0000

    add validation de Spring MVC

--- src/main/java/org/example/divintosrpingmvc/entities/Patient.java
+++ src/main/java/org/example/divintosrpingmvc/entities/Patient.java
@@ -1,8 +1,12 @@
 package org.example.divintosrpingmvc.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import jdk.jfr.Enabled;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -11,9 +15,13 @@ import java.util.Date;
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min = 5, max = 15)
     private String nom;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
+    @DecimalMin("100")
     private boolean malade;
     private int score;
 }
