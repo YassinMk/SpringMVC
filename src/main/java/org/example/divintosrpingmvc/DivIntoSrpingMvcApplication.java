374a4c03af8fee26df2eae6cb1060bc62b99f4c4
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-16 12:34:51 +0000

    add Password Encoder to hasher password

--- src/main/java/org/example/divintosrpingmvc/DivIntoSrpingMvcApplication.java
+++ src/main/java/org/example/divintosrpingmvc/DivIntoSrpingMvcApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 import java.util.stream.Stream;
@@ -21,6 +23,10 @@ public class DivIntoSrpingMvcApplication {
         return args -> {
 
         };
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
