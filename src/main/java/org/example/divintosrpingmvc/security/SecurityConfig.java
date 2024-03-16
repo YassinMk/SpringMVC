5e5a3872930cf48c6f782d2a5041533e3d6ef69e
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-16 16:54:14 +0000

    add JdbcUserDetail Manager for create Roles and Users in our database

--- src/main/java/org/example/divintosrpingmvc/security/SecurityConfig.java
+++ src/main/java/org/example/divintosrpingmvc/security/SecurityConfig.java
@@ -9,11 +9,18 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
         return new InMemoryUserDetailsManager(
